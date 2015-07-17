7c0722b087c327baa37d3b7521083e3a1eea74d6
msmichellegar <msmichellegar@gmail.com>
2015-07-17 11:48:31 +0100

    [MG + NLKB + IT] swaps onkeyup for onclick

--- public/js/todo-list.js
+++ public/js/todo-list.js
@@ -1,4 +1,4 @@
-riot.tag('todo-list', '<div> <ul> <li each="{todos, key in todos}"><input type="checkbox" class="checkbox" id="{todos.key}" onkeyup="{markDone}">{todos.todo}</li> </ul> </div>', function(opts) {
+riot.tag('todo-list', '<div> <ul> <li each="{todos, key in todos}"><input type="checkbox" class="checkbox" id="{todos.key}" onclick="{markDone}">{todos.todo}</li> </ul> </div>', function(opts) {
     this.on('mount', function(){
       console.log("mounted. The value of todos:",todos);
     });
